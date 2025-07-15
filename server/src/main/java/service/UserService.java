62ee7e15f8463d595de7ee073ea49db9546e106a
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-15 11:19:44 -0600

    Implemented the UserService.

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,36 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
+public class UserService {
+
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public AuthData login(UserData user) throws DataAccessException {
+        UserData existingUser = userDAO.getUser(user.username());
+        if (!existingUser.password().equals(user.password())) {
+            throw new DataAccessException("Error. Password already exists.");
+        }
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, user.username());
+        authDAO.createAuth(authData);
+        return authData;
+    }
+
+    public void logout(String authToken) throws DataAccessException {
+        authDAO.getAuth(authToken);
+        authDAO.deleteAuth(authToken);
+    }
+
+}
