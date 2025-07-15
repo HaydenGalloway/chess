fe78fa0a7f270fe424603f84c33f5a83ccb03330
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-15 17:53:41 -0600

    RegisterHandler implemented, Server includes register user endpoint.

---
+++ server/src/main/java/server/RegisterHandler.java
@@ -0,0 +1,41 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import service.UserService;
+import spark.*;
+
+import java.util.Map;
+
+public class RegisterHandler {
+
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public RegisterHandler(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Object handleRegister(Request request, Response response) {
+        try {
+            UserData user = gson.fromJson(request.body(), UserData.class);
+            if (user.username() == null || user.password() == null || user.email() == null) {
+                response.status(400);
+                return gson.toJson(Map.of("message", "Error. Missing registration information."));
+            }
+            AuthData authData = userService.register(user);
+            response.status(200);
+            return gson.toJson(authData);
+        } catch (DataAccessException e) {
+            if ("Error. Username already taken.".equals(e.getMessage())) {
+                response.status(403);
+            } else {
+                // Not sure what could trigger this yet, but maybe when I implement the database.
+                response.status(500);
+            }
+            return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,13 @@
 package server;
 
+import dataaccess.AuthDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import service.UserService;
 import spark.*;
+
+import java.rmi.registry.Registry;
 
 public class Server {
 
@@ -9,11 +16,17 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        UserService userService = new UserService(userDAO, authDAO);
+
+        RegisterHandler registerHandler = new RegisterHandler(userService);
+
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", registerHandler::handleRegister);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
-
+        //Spark.init();
         Spark.awaitInitialization();
         return Spark.port();
     }
