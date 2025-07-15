1610f0982b0b88e8f11177a47b243c8999c1e43b
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-14 21:46:06 -0600

    UserDAO Interface and MemoryUserDAO implementation.

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDAO implements UserDAO {
+
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("Error. Username already taken.");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        UserData user = users.get(username);
+        if (user == null) {
+            throw new DataAccessException("Error. Not authorized.");
+        }
+        return user;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+}
