45cbdb1f1cd4b2bfb7ca36132a2d0cb095f0ecf7
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-15 21:52:10 -0600

    First unit test for clear application endpoint.

---
+++ server/src/main/java/service/ClearServiceTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ClearServiceTest {
+
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private ClearService clearService;
+
+    @BeforeEach
+    public void setClearTestData() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        clearService = new ClearService(userDAO, authDAO, gameDAO);
+    }
+
+    @Test
+    public void clearAllDataTest() throws DataAccessException {
+        userDAO.createUser(new UserData("Kieth123", "KiethIsSuperCoool", "Kieth99@icloud.com"));
+        assertDoesNotThrow(() -> clearService.clearAllData());
+        assertThrows(DataAccessException.class, () -> userDAO.getUser("Kieth123"));
+    }
+}
