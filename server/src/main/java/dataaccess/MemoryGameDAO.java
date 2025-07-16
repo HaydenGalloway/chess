4ee56461551a5cc392e052cf1669dabe9b2ba26e
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-15 19:29:20 -0600

    Started implementing GameDAO to start on the clear application endpoint.

---
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -0,0 +1,16 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryGameDAO implements GameDAO {
+
+    private final Map<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        games.clear();
+    }
+}
