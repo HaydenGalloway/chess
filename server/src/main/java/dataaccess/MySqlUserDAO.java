63ac5194238405a5acc7a3296a6c805f1ac1ba9a
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-21 14:24:22 -0600

    Started implementing MySqlUserDAO with configureDatabase method.

---
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -0,0 +1,46 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.sql.*;
+
+public class MySqlUserDAO implements UserDAO {
+
+    public MySqlUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    private final String[] createStatements = {
+            """
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+}
