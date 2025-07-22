a084c622ca9acbe9196b7ef59d94885ffdba6cbc
HaydenGalloway <connectwithhayden@gmail.com>
2025-07-22 15:42:22 -0600

    GameDAOTests getAllGames, updateGame, clear.

--- server/src/main/java/dataaccess/GameDAOTests.java
+++ server/src/main/java/dataaccess/GameDAOTests.java
@@ -5,6 +5,8 @@ import model.GameData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameDAOTests {
@@ -32,5 +34,41 @@ public class GameDAOTests {
                 "We expect there to be an exception for a null gameName.");
     }
 
+    @Test
+    public void getAllGamesPositive() throws DataAccessException {
+        gameDAO.createGame(new GameData(1, null, "Fred", "FirstGame", new ChessGame()));
+        gameDAO.createGame(new GameData(2, "George", null, "SecondGame", new ChessGame()));
+        gameDAO.createGame(new GameData(3, "Sally", "Nancy", "ThirdGame", new ChessGame()));
+        Collection<GameData> games = gameDAO.getAllGames();
+        assertEquals(3, games.size(), "We expect a collection of three games.");
+    }
+
+    @Test
+    public void getAllGamesNegative() throws DataAccessException {
+        Collection<GameData> games = gameDAO.getAllGames();
+        assertNotNull(games, "Empty collection, but not null.");
+        assertNotEquals(2, games.size(), "No games were added.");
+    }
+
+    @Test
+    public void updateGamePositive() throws DataAccessException {
+        GameData gameData = gameDAO.createGame(new GameData(1, null, null,
+                "OriginalGame", new ChessGame()));
+        int gameID = gameData.gameID();
+        GameData updatedGame = new GameData(gameID, "whiteUsername", "blackUsername",
+                "Original Game", gameData.game());
+        gameDAO.updateGame(gameID, updatedGame);
+        GameData newGameData = gameDAO.getGame(gameID);
+        assertEquals("whiteUsername", newGameData.whiteUsername(), "White user name should be updated.");
+        assertEquals("blackUsername", newGameData.blackUsername(), "Black user name should be updated.");
+    }
+
+    @Test
+    public void clearPositive() throws DataAccessException {
+        gameDAO.createGame(new GameData(1, "whiteUsername", "blackUsername",
+                "Original Game", new ChessGame()));
+        gameDAO.clear();
+        assertTrue(gameDAO.getAllGames().isEmpty(), "We expect an empty collection.");
+    }
 
 }
